cfdaff8025411c1676db558b0a024e7b1e7b57dd
m0a00tt <mudit.arora@walmartlabs.com>
2018-05-06 17:37:47 +0530

    adding term search with 300ms debounce

--- src/Components/search_bar.js
+++ src/Components/search_bar.js
@@ -7,11 +7,15 @@ class SearchBar extends Component {
             term: ''
         };
     }
- 
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
     render() {
         return(
         <div className="search-bar">
-            <input value={this.state.term} onChange={event => this.setState({term: event.target.value})}/>
+            <input value={this.state.term}
+            onChange={event => this.onInputChange(event.target.value) }/>
         </div>
     );
     }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './Components/search_bar';
@@ -15,18 +16,23 @@ class App extends Component {
             videos: [],
             selectedVideo: null,
         };
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            console.log(videos);
-            this.setState({ videos: videos,
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+            videos: videos,
             selectedVideo: videos[0]
             });
         });
     }
 
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
         return(
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetails video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo}) } 
