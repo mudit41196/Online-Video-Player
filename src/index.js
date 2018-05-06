23538110ae207da9f98e3b21433c57ca7441ffbe
m0a00tt <mudit.arora@walmartlabs.com>
2018-05-06 16:04:25 +0530

    adding selectedVideo state

--- src/index.js
+++ src/index.js
@@ -12,11 +12,14 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            videos: []
+            videos: [],
+            selectedVideo: null,
         };
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             console.log(videos);
-            this.setState({ videos });
+            this.setState({ videos: videos,
+            selectedVideo: videos[0]
+            });
         });
     }
 
@@ -24,8 +27,11 @@ class App extends Component {
         return(
             <div>
                 <SearchBar />
-                <VideoDetails video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos} />
+                <VideoDetails video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) } 
+                    videos={this.state.videos} 
+                />
             </div>
         );
     }
