17beafc635499d848cb56427ceb5151aa54d7113
m0a00tt <mudit.arora@walmartlabs.com>
2018-05-06 15:41:33 +0530

    youtube iframe done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './Components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './Components/video_list';
+import VideoDetails from './Components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyDWPpHQgifJpwjHZ_8NLLbXyYnZ1AKMuUE";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: []
+        };
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            console.log(videos);
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <SearchBar />
+                <VideoDetails video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
